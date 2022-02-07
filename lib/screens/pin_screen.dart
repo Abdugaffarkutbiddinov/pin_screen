0cc58675be734d5cf9a3e5528ec229b674e4bfa8
abdugaffar <abdugaffarkutbiddinov@gmail.com>
2022-02-07 19:00:09 +0300

    first app commit

---
+++ lib/screens/pin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PinScreen extends StatefulWidget {
+  const PinScreen({Key? key}) : super(key: key);
+
+  @override
+  _PinScreenState createState() => _PinScreenState();
+}
+
+class _PinScreenState extends State<PinScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
